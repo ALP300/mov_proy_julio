7a6b15499f70319fb7fb9e0665c38a530de74d39
ALP300 <aitorpalomino1@gmail.com>
2025-07-21 21:03:09 -0500

    Actualizar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {dirname, join} from 'path'
 import {fileURLToPath} from 'url'
 import router from './routes/index.js'
+import { conectar  } from './public/services/conexion.js'
 
 const app= express()
 
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('index', {title: 'Home'})
 })
+conectar()
 app.use(router)
 
 app.listen(3000)
---
+++ src/public/services/conexion.js
@@ -0,0 +1,24 @@
+import pg from 'pg';
+const { Client } = pg;
+
+const config = {
+    user: 'db_movies2_user',
+    host: 'dpg-d1vedpk9c44c73dpq2u0-a.oregon-postgres.render.com',
+    database: 'db_movies2',
+    password: 'InF3YWpevemuHLbhHpIs5fRnBCrRmmaE',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+}
+export async function conectar() {
+    const cliente = new Client(config);
+    try {
+        await cliente.connect();
+        console.log('Conexión exitosa a la base de datos');
+        return cliente;
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        throw error;
+    }
+}
